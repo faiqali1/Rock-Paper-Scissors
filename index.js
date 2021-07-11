7b95386aedd10f09dded94e66fc384651af472c7
faiqali1 <74228240+faiqali1@users.noreply.github.com>
2021-07-11 17:54:58 +0300

    Added ComputerPlay Function

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function computerPlay() {
+
+    let options = ["rock", "paper", "scissor"] ;
+
+                // gives a value between 0 and 2  
+    return options[Math.floor(Math.random() * 3 )]; 
+}
+
+
+
+
+
+
+
