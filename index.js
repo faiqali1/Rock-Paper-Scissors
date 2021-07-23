c955dbe372f8df391a4ac7a0945835d9923d6bae
faiqali1 <74228240+faiqali1@users.noreply.github.com>
2021-07-23 18:37:19 +0300

    Completed Project

--- index.js
+++ index.js
@@ -1,88 +1,98 @@
-// console.log("hello world");
-
-
+let humanWins = 0;
+let botWins = 0;
+let round = 1;
 
 
 function computerPlay() {
+  let options = ["rock", "paper", "scissor"];
 
-    let options = ["rock", "paper", "scissor"] ;
-
-                // gives a value between 0 and 2  
-    return options[Math.floor(Math.random() * 3 )]; 
+  // gives a value between 0 and 2
+  return options[Math.floor(Math.random() * 3)];
 }
 
+function playRound(human, bot) {
+  if (human == bot) return "draw...";
 
-function playRound (human , bot){
-
-if (human == bot) return "draw..." ;
-
-if (human == "rock" && bot == "scissor" || human == "scissor" && bot == "paper" || human == "paper" && bot == "rock"  ) return "You Won!";
-else return "you lost...";
-
+  if (
+    (human == "rock" && bot == "scissor") ||
+    (human == "scissor" && bot == "paper") ||
+    (human == "paper" && bot == "rock")
+  )
+    return "You Won!";
+  else return "you lost...";
 }
 
+function game(userChoice) {
+
+   if (humanWins == 5 || botWins == 5) {
+     alert("Reload the page to play again");
+     return;
+  }
+    let human = userChoice;
+    let bot = computerPlay();
+    UpdateComputerPicture(bot);
+    document.getElementById("number").innerHTML = round;
 
 
-function game (){
-    let humanWins =0;
-    let botWins =0;
-        
-    while(humanWins!=5 || botWins!= 5){
+    // console.log("Computer Chose: " + bot + "\n");
 
-        if (humanWins ==5 || botWins == 5) break;
-
-        let human = "paper";
-        let bot = computerPlay();
-        // console.log("Computer Chose: " + bot + "\n");
-        
-        if (playRound(human,bot) == "You Won!") humanWins++;
-        else if (playRound(human,bot) == "draw...") continue;
-        else botWins++;
-
-        // console.log("\n"+ "humanwins :" + humanWins);
-        // console.log("botwins :" +botWins);
+        // incrementing the winner
+    if (playRound(human, bot) == "You Won!"){ 
+        humanWins++
+        document.getElementById("user-wins").innerHTML = humanWins;
+        document.getElementById("result").innerHTML = "You win...";
+    } 
+    else if (playRound(human, bot) == "draw..."){
+        document.getElementById("result").innerHTML = "Draw....";
+    } 
+    else { // bot wins...
+        botWins++;
+        document.getElementById("computer-wins").innerHTML = botWins;
+        document.getElementById("result").innerHTML = "You lose...";
     }
+    round++;
+    // console.log("\n"+ "humanwins :" + humanWins);
+    // console.log("botwins :" +botWins);
 
-    if (humanWins == 5) return "humans win";
-    else if (botWins == 5) return "bots win";
-    else return "error";
+
+   // document.getElementById("result").innerHTML = "Game Over You win!! ";
+  if (humanWins == 5) return document.getElementById("result").innerHTML = "Game Over You win!! ";
+  else if (botWins == 5) return document.getElementById("result").innerHTML = "Game Over try again!! ";
+  else return "error";
 }
 
-console.log(game());
+function UpdateComputerPicture(CompChoice) {
+ // let CompChoice = computerPlay();
+
+  switch (CompChoice) {
+    case "rock":
+      document.getElementById("computer-img").src =
+        "/Assets/mycollection(pure)/svg/002-rock.svg";
+      break;
+    case "paper":
+      document.getElementById("computer-img").src =
+        "/Assets/mycollection(pure)/svg/001-file.svg";
+      break;
+    case "scissor":
+      document.getElementById("computer-img").src =
+        "/Assets/mycollection(pure)/svg/003-scissors.svg";
+      break;
+
+    default:
+      console.log("error");
+  }
+}
+
+// console.log(game());
+
+// document.getElementById("computer-img").addEventListener("click", UpdateComputerPicture);
 
 
+function userInput( user ) {
+    userChoice=user;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function setGameState() {
+    
+// }
 
